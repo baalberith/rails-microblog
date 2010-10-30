592dcc8f0cf4d39b70a3e6f559e36962c599f351
baalberith <baal6erith@gmail.com>
2010-10-30 17:57:28 +0200

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,18 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
     @page_title = "Sign up"
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Wlecome to you're Microblog"
+      redirect_to @user
+    else
+      @page_title = "Sign up"
+      render :new
+    end
   end
 end
