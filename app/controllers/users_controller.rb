e17073d43ebaaea66917fa7422cc9dadbc9bb94d
baalberith <baal6erith@gmail.com>
2010-10-30 23:50:33 +0200

    admin can't delete admin account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,30 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :only => [:index, :edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
-  before_filter :admin_user,   :only => :destroy
+  before_filter :admin_user, :only => :destroy
+  before_filter :signed_out_user, :only => [:new, :create]
   
   def index
     @users = User.all
+    
     @page_title = 'All users'
   end
   
   def show
     @user = User.find(params[:id])
+    
     @page_title = 'Profile'
   end
   
   def new
     @user = User.new
+    
     @page_title = 'Sign up'
   end
   
   def create
     @user = User.new(params[:user])
+    
     if @user.save
       sign_in @user
       flash[:success] = "Wlecome to you're Microblog, #{@user.name}!"
@@ -47,8 +52,15 @@ class UsersController < ApplicationController
   end
   
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
+    user = User.find(params[:id])
+    
+    if user.admin?
+      flash[:error] = "Admin user can't be deleted."
+    else
+      user.destroy
+      flash[:success] = "User destroyed."
+    end
+    
     redirect_to users_path
   end
   
@@ -64,5 +76,9 @@ class UsersController < ApplicationController
     
     def admin_user
       redirect_to root_path unless current_user.admin?
+    end
+    
+    def signed_out_user
+      redirect_to root_path if signed_in?
     end
 end
