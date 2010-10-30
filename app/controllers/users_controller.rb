e12dd11de2fc794c2891053a499ceea50a395ea2
baalberith <baal6erith@gmail.com>
2010-10-30 16:59:01 +0200

    User model with passwords, and user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
     @page_title = "Profile"
-    @user = User.find params[:id]
   end
   
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
+require 'digest'
+
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
   
   email_regex = /\A[a-z\d_+.-]+@[a-z\d.-]+\.[a-z]{2,4}\z/i
   
@@ -8,4 +11,36 @@ class User < ActiveRecord::Base
   validates :email, :presence => true,
     :format => { :with => email_regex },
     :uniqueness => { :case_sensitive => false }
+  validates :password, :presence => true,
+    :confirmation => true,
+    :length => { :within => 6..20 }
+  
+  before_save :encrypt_password
+  
+  def valid_password?(submitted_password)
+    self.encrypted_password == encrypt(submitted_password)
+  end
+  
+  def self.authenticate(email, submitted_password)
+    user = User.find_by_email(email)
+    user && user.valid_password?(submitted_password) ? user : nil
+  end
+  
+  private
+    def encrypt_password
+      self.salt = make_salt if self.new_record?
+      self.encrypted_password = encrypt(self.password)
+    end
+    
+    def encrypt(string)
+      secure_hash "#{salt}--#{string}"
+    end
+    
+    def make_salt
+      secure_hash "#{Time.now.utc}--#{self.password}"
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
